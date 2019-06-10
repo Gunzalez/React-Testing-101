5cc6e0486845a110576daa48825979950310ca12
Segun Konibire <gunzalez@hotmail.com>
2019-06-10 18:05:58 +0100

    Minor refactor

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,8 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const GuessedWords = props => {
-  return <div data-test="component-guessedwords" />;
+const GuessedWords = ({ guessedWords }) => {
+  let content;
+  if (guessedWords.length) {
+    return <div data-test="component-guessedwords">{content}</div>;
+  } else {
+    return (
+      <div data-test="component-guessedwords">
+        <span data-test="guess-instructions">Try to guess the word!</span>
+      </div>
+    );
+  }
 };
 
 GuessedWords.propTypes = {
--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -3,7 +3,6 @@ import { shallow } from "enzyme";
 import { findByAttr, checkProps } from "./test/test-utils";
 
 import GuessedWords from "./GuessedWords";
-import { stat } from "fs";
 
 const defaultProps = {
   guessedWords: [{ guessedWord: "mouse", lettersMatchCount: 3 }]
@@ -13,13 +12,25 @@ const setup = (props = {}, state = null) => {
   const setUpProps = { ...defaultProps, ...props };
   return shallow(<GuessedWords {...setUpProps} />);
 };
-
-test("renders without crashing", () => {
-  const wrapper = setup();
-  const appComponent = findByAttr(wrapper, "component-guessedwords");
-  expect(appComponent.length).toBe(1);
-});
-
 test("does not throw error with expected props ", () => {
   checkProps(GuessedWords, defaultProps);
 });
+
+describe("if there are not words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+
+  test("renders without crashing", () => {
+    const appComponent = findByAttr(wrapper, "component-guessedwords");
+    expect(appComponent.length).toBe(1);
+  });
+
+  test("renders instructions to guess a word", () => {
+    const guessEl = findByAttr(wrapper, "guess-instructions");
+    expect(guessEl.text().length).not.toBe(0);
+  });
+});
+
+describe("if there are words guessed", () => {});
